f4377e0b7431af720dcd5e2ba8ed9827b4029a32
SuneelOpargo <suneel.kumar@solventek.com>
2026-07-14 21:58:40 +0530

    Added the headless is true to workin cloud server in all files

--- app/connectors/base_connector.py
+++ app/connectors/base_connector.py
@@ -29,7 +29,7 @@ class BaseConnector:
     def __init__(
         self,
         connector_name: str,
-        headless: bool = TRue,
+        headless: bool = True,
         timeout: int = 30000,
         slow_mo: int = 0,
     ):
